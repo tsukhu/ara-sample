668af92be4e99783b270d4e64e2b52db4a1f924a
Tarun Kumar Sukhu <tksukhu@gmail.com>
2020-04-30 11:45:34 +0530

    updated svelte example

--- nova/src/components/Example.svelte
+++ nova/src/components/Example.svelte
@@ -1,24 +1,28 @@
 <script>
-	import { onMount, onDestroy} from 'svelte';
-	export let title;
-
-
-  onMount(async () => {
-	  if (typeof document !== "undefined") {
-  		document.addEventListener("change-title", e => {
-		console.log('EVENT',e);
-		if (typeof e.detail !== "undefined") {
-		title = e.detail;
-		}
-	})
-	  }
-	}); 
-
-  onDestroy(() => { if (typeof document !== "undefined") { document.removeEventListener("change-title")}});
-
+  import Scatterplot from './Scatterplot.svelte';
+  import data from '../data.js';
+  export let title;
 </script>
 
-<div>
- <h1>Hello {title}</h1>
- <input type="text" bind:value={title}>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+  .chart {
+    width: 100%;
+    max-width: 640px;
+    height: calc(100% - 4em);
+    min-height: 280px;
+    max-height: 480px;
+    margin: 0 auto;
+    font-family: 'Roboto', sans-serif;
+    font-size: small;
+  }
+</style>
+
+<div class="chart">
+  <h2>{title}</h2>
+
+  <Scatterplot points={data.a} />
+  <Scatterplot points={data.b} />
+  <Scatterplot points={data.c} />
+  <Scatterplot points={data.d} />
 </div>
